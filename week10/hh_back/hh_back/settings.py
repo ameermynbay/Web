de926ff6b3bcb1835a3c46eb30e4b9c812f84129
Ameer <mynbay01@gmail.com>
2020-04-17 01:28:24 +0600

    added some of the front part

--- week10/hh_back/hh_back/settings.py
+++ week10/hh_back/hh_back/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : ('rest_framework_simplejwt.authentication.JWTAuthentication', )
+}
